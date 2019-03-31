79dc2d54ba492ea4b535d18c2991a8e81bbca952
xkadmx <zuzanna.rosinska@yahoo.com>
2019-03-31 20:25:46 +0200

    setting support v4 (not v7), removing AppCompat, remaining basic Activity class

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.notification;
 
+import android.app.Activity;
 import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationCompat.Builder = new NotificationCompat.Builder(this);
+        NotificationCompat.Builder = new NotificationCompat.Builder(this)
+                .setContentTitle("Notyfikacje na kolaaaacje")
+                .setContentText("Ten tekst pochodzi z Radomia.")
+                .setSmallIcon(R.mipmap.ic_launcher)
+
     }
 }
