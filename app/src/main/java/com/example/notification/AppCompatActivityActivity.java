31e0b079e327cada59969533a0a904adeb33212d
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-02 21:05:44 +0200

    NotificationManager added with method getSystemService

---
+++ app/src/main/java/com/example/notification/AppCompatActivityActivity.java
@@ -0,0 +1,4 @@
+package com.example.notification;
+
+class AppCompatActivityActivity {
+}
--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.notification;
 
+
+
 import android.app.Activity;
-import android.support.v4.app.NotificationCompat;
+import android.app.NotificationManager;
+import android.support.v7.app.NotificationCompat;
 import android.os.Bundle;
 
+
 public class MainActivity extends Activity {
+
+    public String channelId;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationCompat.Builder = new NotificationCompat.Builder(this)
+        NotificationCompat.Builder = new NotificationCompat.Builder(this, String( channelId))
                 .setContentTitle("Notyfikacje na kolaaaacje")
                 .setContentText("Ten tekst pochodzi z Radomia.")
                 .setSmallIcon(R.mipmap.ic_launcher);
-
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(1,builder.build())
     }
 }
